5d4828f15aab0c9ab497811efda75b5a484a4922
adamvaughn <adamdvaughn@gmail.com>
2022-04-20 14:16:55 -0500

    added function to add team member types

--- index.js
+++ index.js
@@ -8,3 +8,34 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const generateTeam = require("./src/template.js")
+
+/* Team Array */
+teamArray = [];
+
+/* App function */
+function runApp () {
+    /* Select Team Make-up*/
+    function createTeam () {
+      inquirer.prompt([{
+        type: "list",
+        message: "What type of employees would you like to add to your team?",
+        name: "addEmployeePrompt",
+        choices: ["Manager", "Engineer", "Intern", "No more employees to add."]
+      }]).then(function (userInput) {
+        switch(userInput.addEmployeePrompt) {
+          case "Manager":
+            addManager();
+            break;
+          case "Engineer":
+            addEngineer();
+            break;
+          case "Intern":
+            addIntern();
+            break;
+  
+          default:
+            htmlBuilder();
+        }
+      })
+    }
+}
