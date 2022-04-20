09f910fe72ff674df86c8bff7f6635fca146a162
adamvaughn <adamdvaughn@gmail.com>
2022-04-20 13:15:47 -0500

    add variables to index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const fs = require('fs'); 
-const inquirer = require('inquirer');
-const Employee = require("./lib/Employee");
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");s
+/* Variables/Required */
+const Manager = require("./lib/Manager.js");
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+const generateTeam = require("./src/template.js")
